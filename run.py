1d7c3041ad07f8ebdcc84b1df76d11a7564a16be
Adetops <heritagediwamu@gmail.com>
2025-12-19 02:06:28 +0100

    updated run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 # Initiates the app
+import eventlet
+eventlet.monkey_patch()
+
 from app import create_app
 from app.extensions import socketio
 from app.simulation.simulation import start_simulation_engine
@@ -8,4 +11,5 @@ app = create_app()
 
 if __name__ == "__main__":
   start_simulation_engine()
-  socketio.run(app, debug=False, allow_unsafe_werkzeug=True)
+  port = int(os.environ.get("PORT", 5000))
+  socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
